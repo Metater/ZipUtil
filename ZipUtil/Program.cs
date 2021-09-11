d0ec972dbe87a4084174634074c4f0a2779a4617
Connor <61102529+Metater@users.noreply.github.com>
2021-09-10 21:29:26 -0400

    Add project

---
+++ ZipUtil/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace ZipUtil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2) return;
+            string zipDir = args[0];
+            string zippedFile = args[1];
+            if (!Directory.Exists(zipDir)) return;
+            if (File.Exists(zippedFile)) return;
+            ZipFile.CreateFromDirectory(zipDir, zippedFile);
+        }
+    }
+}
